1c0c4ec97f5f40cd9ecb779d8f5cfd9b3c7b71f6
io <io@172.22.15.73>
2018-04-11 09:11:47 +0200

    Versione iniziale, realizzata parte grafica e modello iniziale

---
+++ Lab5_Anagrammi/src/it/polito/tdp/model/Model.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.model;
+
+import java.util.*;
+
+public class Model {
+
+	List <String> soluzione=new ArrayList<String>(); //Elenco delle soluzioni
+	List<Character> lettere=new ArrayList<Character>(); //Array di caratteri della parola
+	
+}
